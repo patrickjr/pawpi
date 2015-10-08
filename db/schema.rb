78b31370c36c87b22430876336bae3f0609d6785
patrickjr <patrickjriel@gmail.com>
2015-10-08 02:45:37 +0000

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,45 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150610215626) do
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.integer  "type_id",     limit: 4
+    t.integer  "user_id",     limit: 4
+    t.string   "breed",       limit: 255
+    t.integer  "zipcode",     limit: 4
+    t.integer  "age",         limit: 4
+    t.string   "sex",         limit: 255
+    t.string   "description", limit: 255
+    t.integer  "is_reported", limit: 1
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",              limit: 255
+    t.string   "email",             limit: 255
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.string   "password_digest",   limit: 255
+    t.string   "remember_digest",   limit: 255
+    t.string   "activation_digest", limit: 255
+    t.boolean  "activated",         limit: 1
+    t.datetime "activated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
+
+end
